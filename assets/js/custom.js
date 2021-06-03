fc6bdda5eb81372d3fedb5d0944bf28970e13a35
rheathamahon <rheathamahon@gmail.com>
2021-06-04 04:36:38 +0800

    vvl213

---
+++ assets/js/custom.js
@@ -0,0 +1,42 @@
+$('.owl-one').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:1,
+          nav:false
+      },
+      1000:{
+          items:3,
+          nav:true,
+          loop:false
+      }
+  }
+})
+
+
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+  })
